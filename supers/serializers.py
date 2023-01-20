f839f5c1fd0fe6c79ac6b690d413d703a7d5274f
wenchijchang <heyyhey@hotmail.com>
2023-01-20 11:07:51 -0500

    Created Super Serializer Class and file + Created SuperType Serializer Class and file

---
+++ supers/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Super
+
+class SuperSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Super
+        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type_id']
